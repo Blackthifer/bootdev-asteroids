dbf32139687081e0ac0f4b7b548420bc6f0c0d71
Blackthifer <hessel96@msn.com>
2025-05-01 01:58:27 +0200

    added fps limiter

--- main.py
+++ main.py
@@ -7,12 +7,15 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pg.init()
     screen = pg.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+    fps_timer = pg.time.Clock()
+    dt = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
         screen.fill("black")
         pg.display.flip()
+        dt = fps_timer.tick(FRAMES_PER_SECOND) / 1000
 
 
 if __name__ == "__main__":
