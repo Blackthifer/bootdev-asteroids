a0e4ec76ba055ddb1c03f8982ec086d04ef95e84
Blackthifer <hessel96@msn.com>
2025-05-01 17:28:08 +0200

    updated asteroid spawn logic

--- asteroidfield.py
+++ asteroidfield.py
@@ -5,10 +5,10 @@ from constants import *
 
 class AsteroidField(pg.sprite.Sprite):
     edges = [
-            lambda y: pg.Vector2(-ASTEROID_MAX_RADIUS, y * SCREEN_HEIGHT),
-            lambda y: pg.Vector2(SCREEN_WIDTH + ASTEROID_MAX_RADIUS, y * SCREEN_HEIGHT),
-            lambda x: pg.Vector2(x * SCREEN_WIDTH, -ASTEROID_MAX_RADIUS),
-            lambda x: pg.Vector2(x * SCREEN_WIDTH, SCREEN_HEIGHT + ASTEROID_MAX_RADIUS),
+            lambda y, r: pg.Vector2(-r, y * SCREEN_HEIGHT),
+            lambda y, r: pg.Vector2(SCREEN_WIDTH + r, y * SCREEN_HEIGHT),
+            lambda x, r: pg.Vector2(x * SCREEN_WIDTH, -r),
+            lambda x, r: pg.Vector2(x * SCREEN_WIDTH, SCREEN_HEIGHT + r),
             ]
 
     def __init__(self):
@@ -24,8 +24,8 @@ class AsteroidField(pg.sprite.Sprite):
         if self.spawn_timer >= ASTEROID_SPAWN_RATE:
             self.spawn_timer = 0
             
-            edge = rand.choice(self.edges)
-            position = edge(rand.uniform(0, 1))
-            velocity = pg.Vector2(1, 0).rotate(rand.randrange(360)) * rand.normalvariate(40, 10)
             radius = rand.randint(ASTEROID_MIN_RADIUS, ASTEROID_MAX_RADIUS)
+            edge = rand.choice(self.edges)
+            position = edge(rand.uniform(0, 1), radius)
+            velocity = pg.Vector2(1, 0).rotate(rand.randrange(360)) * rand.normalvariate(40, 10)
             Asteroid(position.x, position.y, velocity.x, velocity.y, radius)
